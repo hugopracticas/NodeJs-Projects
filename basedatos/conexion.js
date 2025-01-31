495fe19c2a6f1214bff42f00f53776f0ce855301
hugopracticas <hugoloyolamaya@gmail.com>
2025-01-31 11:34:13 -0600

    created the conection with the DB, and made the structure of the project

---
+++ basedatos/conexion.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const text = require("../text/textFile.json");
+
+const conexion = async () => {
+  const url = `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.SERVER_DB}/?retryWrites=true&w=majority&appName=Cluster0`;
+  try {
+    await mongoose.connect(url, {});
+    console.log(`${text.dataBase} ${url.substring(url.length - 8)}`);
+  } catch (error) {
+    console.log(error);
+    throw new Error(`${text.errorConectionDB}`);
+  }
+};
+
+module.exports = {
+  conexion,
+};
--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hola que!!#");
+const { conexion } = require("./basedatos/conexion");
+const express = require("express");
+const cors = require("cors");
+
+//Inicializar app
+console.log("App de node arrancada");
+
+//Conectar a la BD
+conexion();
+
+//Crear servidor de Node
+const app = express();
+const puerto = process.env.PORT || 3000;
+
+//Configurar cors
+app.use(cors());
+
+//Convertir body a objeto js
+
+//Crear rutas
+app.get("/probando", (req, res) => {
+  console.log("Se ha ejecutado el endpoint probando");
+
+  return res.status(200).send({});
+});
+
+//Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+  console.log(`Servidor corriendo en puerto ${puerto}`);
+});
