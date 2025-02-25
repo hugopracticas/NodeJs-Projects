9d3e7ed9ee3b44f32e3f36d5171f2fc09d74328c
hugopracticas <hugoloyolamaya@gmail.com>
2025-02-24 21:48:51 -0600

    Started again project

--- routes/publication.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const PublicationController = require("../controllers/publication");
-
-//Definir rutas
-router.get("/publication-test", PublicationController.publicationTest);
-
-//Exportar ruter
-module.exports = router;
